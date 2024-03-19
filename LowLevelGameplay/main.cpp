a54bf3dcb6e4759c78ae63a5cd6edb187e12c9b4
Zeus <88673236+Zeus4113@users.noreply.github.com>
2024-03-19 12:22:03 +0000

    Week 2 Commit 1
    Vector 2 Class
    Sprite Sheet
    Delta Time
    Fixed Update

--- LowLevelGameplay/main.cpp
+++ LowLevelGameplay/main.cpp
@@ -1,24 +1,69 @@
 #include <SFML/Graphics.hpp>
+#include "vector2.h"
+#include <chrono>
+#include <iostream>
 
-int main()
-{
-	sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
-	sf::CircleShape shape(100.f);
-	shape.setFillColor(sf::Color::Green);
+#define FIXEDFRAMERATE 0.02f
 
-	while (window.isOpen())
+//namespace LLGP {
+
+	int main()
 	{
-		sf::Event event;
-		while (window.pollEvent(event))
+		// Initial setup
+		sf::RenderWindow window(sf::VideoMode(990, 540), "SFML works!");
+		std::chrono::steady_clock::time_point lastTime = std::chrono::steady_clock::now();
+		float deltaTime = 0.f;
+		float timeSincePhysicsStep = 0.f;
+
+		// Create Vectors
+		LLGP::Vector2<float> rectSize = LLGP::Vector2<float>::one * 100;
+		LLGP::Vector2<float> rectPos = LLGP::Vector2<float>(495, 270);
+
+		// Create Texture
+		sf::Texture rectTex; rectTex.loadFromFile("images/robotronsprites.png");
+		LLGP::Vector2i spritesInTex(8, 9);
+		LLGP::Vector2i rectTexSize(rectTex.getSize());
+		sf::IntRect rectTexUV(0, (rectTexSize.y / spritesInTex.y) * 5, rectTexSize.x / spritesInTex.x, rectTexSize.y / spritesInTex.y);
+
+		// Create and set sprite data
+		sf::RectangleShape rectangle(rectSize);
+		rectangle.setTexture(&rectTex);
+		rectangle.setTextureRect(rectTexUV);
+		rectangle.setOrigin(rectSize / 2);
+		rectangle.setPosition(rectPos);
+
+		// Update
+		while (window.isOpen())
 		{
-			if (event.type == sf::Event::Closed)
-				window.close();
+			// Update Time
+			std::chrono::steady_clock::time_point now = std::chrono::steady_clock::now();
+			deltaTime = std::chrono::duration_cast<std::chrono::microseconds>(now - lastTime).count() / 1000000.f;
+			lastTime = now;
+
+			// Check closed
+			sf::Event event;
+			while (window.pollEvent(event))
+			{
+				if (event.type == sf::Event::Closed)
+					window.close();
+			}
+
+			// Physics Update
+			timeSincePhysicsStep += deltaTime;
+			while (timeSincePhysicsStep > FIXEDFRAMERATE)
+			{
+				std::cout << "Time since last step:" << timeSincePhysicsStep << "\n";
+				//std::cout << "Current Time: " << deltaTime << "\n";
+				timeSincePhysicsStep -= FIXEDFRAMERATE;
+			}
+
+			// Render
+			window.clear();
+			window.draw(rectangle);
+			window.display();
 		}
 
-		window.clear();
-		window.draw(shape);
-		window.display();
+		return 0;
 	}
 
-	return 0;
-}
+//}
---
+++ LowLevelGameplay/vector2.h
@@ -0,0 +1,168 @@
+#pragma once
+#include "arithmetic_concept.h"
+#include <SFML/Graphics.hpp>
+
+namespace LLGP {
+	
+	template<typename T> requires arithmatic<T>
+
+	struct Vector2 {
+	public:
+		T x, y;
+
+#pragma region constructors
+		Vector2() : x(0), y(0) {}
+		Vector2(const Vector2&) = default;
+		Vector2(T _x, T _y) : x(_x), y(_y) {}
+
+		template<typename U> requires arithmatic<U>
+		explicit Vector2(const Vector2<U>& in) :
+			x(static_cast<T>(in.x)), y(static_cast<T>(in.y)) {}
+		
+		template<typename U> requires arithmatic<U>
+		explicit Vector2(const sf::Vector2<U>& in) :
+			x(static_cast<T>(in.x)), y(static_cast<T>(in.y)) {}
+
+		template<typename U> requires arithmatic<U>
+		operator sf::Vector2<U>() { return sf::Vector2<U>(static_cast<U>(x), static_cast<U>(y)); }
+
+#pragma endregion
+
+		float GetSqrMagnitude() { return x * x + y * y; }
+		float GetMagnitude() { return sqrt(GetSqrMagnitude()); }
+		Vector2<T>& Normalise() {
+			*this /= GetMagnitude(); return *this;
+		}
+		Vector2<T> Normalised() {
+			return *this / GetMagnitude(); return*this;
+		}
+		static float Dot(const Vector2<T>& lhs, const Vector2<T>& rhs)
+		{
+			return (float)(lhs.x * rhs.x + lhs.y * lhs.y);
+		}
+		static float Angle(Vector2<T>& lhs, Vector2<T>& rhs)
+		{
+			return acos(Dot(lhs.Normalised(), rhs.Normalised()));
+		}
+
+		static const Vector2 zero;
+		static const Vector2 one;
+		static const Vector2 up;
+		static const Vector2 down;
+		static const Vector2 left;
+		static const Vector2 right;
+
+	};
+
+	// Setter Operators
+
+	template<typename T> requires arithmatic<T>
+	Vector2<T>& operator+=(Vector2<T>& lhs, const Vector2<T> rhs)
+	{
+		lhs.x += rhs.x; lhs.y += rhs.y; return lhs;
+	}
+
+	template<typename T> requires arithmatic<T>
+	Vector2<T>& operator-=(Vector2<T>& lhs, Vector2<T> rhs) 
+	{
+		lhs.x -= rhs.x; lhs.y -= rhs.y; return lhs;
+	}
+
+	template<typename T, typename U> requires arithmatic<T> and arithmatic<U>
+	Vector2<T>& operator*=(Vector2<T>& v, const U a)
+	{
+		v.y *= a; v.x *= a; return v;
+	}
+
+	template<typename T, typename U> requires arithmatic<T> and arithmatic<U>
+	Vector2<T>& operator/=(Vector2<T>& v, const U a)
+	{
+		v.y /= a; v.x /= a; return v;
+	}
+
+	// Calculation Operators
+
+	template<typename T> requires arithmatic<T>
+	Vector2<T> operator-(const Vector2<T> v)
+	{
+		return Vector2<T>(-v.x, -v.y);
+	}
+
+	template<typename T> requires arithmatic<T>
+	Vector2<T> operator-(const Vector2<T> lhs, const Vector2<T>& rhs)
+	{
+		return lhs -= rhs;
+	}
+
+	template<typename T> requires arithmatic<T>
+	Vector2<T> operator+(Vector2<T> lhs, const Vector2<T>& rhs)
+	{
+		return lhs += rhs;
+	}
+
+	template<typename T, typename U> requires arithmatic<T> and arithmatic<U>
+	Vector2<T> operator/(Vector2<T> v, const U a)
+	{
+		return v /= a;
+	}
+
+	template<typename T, typename U> requires arithmatic<T> and arithmatic<U>
+	Vector2<T> operator/(const U a, Vector2<T> v)
+	{
+		return v /= a;
+	}
+
+	template<typename T, typename U> requires arithmatic<T> and arithmatic<U>
+	Vector2<T> operator*(Vector2<T> v, const U a)
+	{
+		return v *= a;
+	}
+
+	template<typename T, typename U> requires arithmatic<T> and arithmatic<U>
+	Vector2<T> operator*(const U a, Vector2<T> v)
+	{
+		return v *= a;
+	}
+
+	// Comparison Operators
+
+	template<typename T> requires arithmatic<T>
+	inline bool operator==(Vector2<T>& lhs, const Vector2<T> rhs)
+	{
+		Vector2<T> dist = lhs - rhs;
+		float mag = dist.x * dist.x + dist.y * dist.y;
+		return mag < 9.99999944E-11f;
+	}
+
+	template<typename T> requires arithmatic<T>
+	inline bool operator!=(Vector2<T>& lhs, const Vector2<T> rhs)
+	{
+		return !(lhs == rhs);
+	}
+
+
+	// Static References
+	template<typename T> requires arithmatic<T>
+	const Vector2<T> Vector2<T>::zero(static_cast<T>(0), static_cast<T>(0));
+
+	template<typename T> requires arithmatic<T>
+	const Vector2<T> Vector2<T>::one(static_cast<T>(1), static_cast<T>(1));
+
+	template<typename T> requires arithmatic<T>
+	const Vector2<T> Vector2<T>::left(static_cast<T>(-1), static_cast<T>(0));
+
+	template<typename T> requires arithmatic<T>
+	const Vector2<T> Vector2<T>::right(static_cast<T>(1), static_cast<T>(0));
+
+	template<typename T> requires arithmatic<T>
+	const Vector2<T> Vector2<T>::up(static_cast<T>(0), static_cast<T>(1));
+
+	template<typename T> requires arithmatic<T>
+	const Vector2<T> Vector2<T>::down(static_cast<T>(0), static_cast<T>(-1));
+
+
+	// Type definitions
+	typedef Vector2<int> Vector2i;
+	typedef Vector2<unsigned int> Vector2u;
+	typedef Vector2<double> Vector2d;
+}
